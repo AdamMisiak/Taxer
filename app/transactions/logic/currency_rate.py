01edc8290a9868620e9174c3eba65dee28c065fe
Adam Misiak <adammisiak3@gmail.com>
2024-01-11 13:41:06 +0100

    Add CHF to the currencies

--- app/transactions/logic/currency_rate.py
+++ app/transactions/logic/currency_rate.py
@@ -12,12 +12,14 @@ def save_data_currency_rates_file(file):
         if len(row) == 0 or not re.match(r"^([\d]+)$", row[0]):
             continue
 
+        # NOTE sprawdzic czy na pewno dobrze zapisuje waluty - porownac ze strona jakas inna kursy
         CurrencyRate.objects.get_or_create(
             date=datetime.strptime(row[0], "%Y%m%d"),
             defaults={
                 "usd": float(row[2].replace(",", ".")),
                 "eur": float(row[8].replace(",", ".")),
                 "gbp": float(row[11].replace(",", ".")),
+                "chf": float(row[10].replace(",", ".")),
                 "rub": float(row[30].replace(",", ".")) if row[30] else None,
             },
         )
