8d1f44fcdb119d3187bb0a89c5159585ccdea95d
Adam Misiak <adammisiak3@gmail.com>
2024-03-02 16:26:36 +0100

    Upgrade Report files model in new app

--- app/files/admin.py
+++ app/files/admin.py
@@ -3,6 +3,16 @@ from files.models import ReportFile
 
 
 class ReportFileAdmin(admin.ModelAdmin):
-    list_display = ("id", "file", "created_at")
+    list_display = ("id", "file", "user", "created_at")
+    readonly_fields=('user',)
+
+    def save_model(self, request, instance, form, change):
+        user = request.user 
+        instance = form.save(commit=False)
+        if not change or not instance.user:
+            instance.user = user
+        instance.user = user
+        instance.save()
+        return instance
 
 admin.site.register(ReportFile, ReportFileAdmin)
--- app/files/models/report_file.py
+++ app/files/models/report_file.py
@@ -1,13 +1,13 @@
 from django.db import models
+from django.conf import settings
 from utils.models import Broker
 
 class ReportFile(models.Model):
     file = models.FileField()
-    # NOTE user
-    # owner = models.ForeignKey(
-    #     settings.AUTH_USER_MODEL,
-    #     on_delete=models.CASCADE,
-    # )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
     broker = models.ForeignKey(Broker, blank=True, null=True, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
