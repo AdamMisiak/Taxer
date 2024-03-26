0a4e6efda39edcf241e82ac9ee84e1aa11f49ca1
Adam Misiak <adammisiak3@gmail.com>
2024-03-26 19:05:38 +0100

    Update AssetTransaction model

--- app/transactions/models/asset_transaction.py
+++ app/transactions/models/asset_transaction.py
@@ -11,27 +11,28 @@ from transactions.models import BaseTransaction
 
 
 class AssetTransaction(BaseTransaction):
-    # NOTE why null and blank?
-    price = models.FloatField(null=True, blank=True)
-    fee = models.FloatField(null=True, blank=True)
-    quantity = models.FloatField(null=True, blank=True)
+    price = models.FloatField()
+    fee = models.FloatField()
+    quantity = models.FloatField()
 
     value = models.FloatField()
-    full_value = models.FloatField(db_comment="Value of assets including fee", null=True, blank=True)
+    full_value = models.FloatField(db_comment="Value of assets including fee")
 
     value_pln = models.FloatField()
-    full_value_pln = models.FloatField(db_comment="Value of assets including fee in PLN", null=True, blank=True)
+    full_value_pln = models.FloatField(db_comment="Value of assets including fee in PLN")
 
     class Meta:
         verbose_name = "Asset transaction"
         verbose_name_plural = "Asset transactions"
         unique_together = ("asset_name", "side", "price", "quantity", "executed_at")
 
-    # def __str__(self):
-    #     print([f.name for f in AssetTransaction._meta.get_fields()])
-    #     return f"{AssetTransaction.__name__} - {self.side} {self.quantity} {self.asset_name} @ {self.price} {self.currency} - {self.executed_at.date()}"
+    def __str__(self):
+        return f"{AssetTransaction.__name__} - {self.side} {self.quantity} {self.asset_name} @ {self.price} {self.currency} - {self.executed_at.date()}"
 
     def save(self, *args, **kwargs):
+        #NOTE a co jak bedize ETF?
+        self.asset_type = AssetType.STOCKS.value
+
         if self.id is not None:
             print(f"♻️  Updated: {self}\n")
         else:
