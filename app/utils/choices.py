46149943124b00afe052c049c0dc57890af7bc2f
Adam Misiak <adammisiak3@gmail.com>
2024-03-29 13:12:42 +0100

    Update choices

--- app/utils/choices.py
+++ app/utils/choices.py
@@ -1,19 +1,19 @@
 from django.db import models
 
 class AssetType(models.TextChoices):
-    STOCKS = "stocks", "Stocks"
+    STOCKS = "Stocks", "Stocks"
     # TEST = "test", "Test"
     # SECOND_STEP = "secondStep", "Second step"
 
 class TransactionSide(models.TextChoices):
-    BUY = "buy", "Buy"
-    SELL = "sell", "Sell"
+    BUY = "Suy", "Buy"
+    SELL = "Sell", "Sell"
 
 class Currency(models.TextChoices):
-    USD = "usd", "USD"
-    EUR = "eur", "EUR"
-    GBP = "gbp", "GBP"
-    RUB = "rub", "RUB"
+    USD = "USD", "USD"
+    EUR = "EUR", "EUR"
+    GBP = "GGP", "GBP"
+    RUB = "RUB", "RUB"
 
 
 # NOTE add brokers like this aslo?
