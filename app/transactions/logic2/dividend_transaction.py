1906aa590cbe0469ed80b87e668ce214aac21a44
Adam Misiak <adammisiak3@gmail.com>
2024-04-12 11:33:53 +0200

    Upgrade save_ib_lynx_dividend_transaction script

--- app/transactions/logic2/dividend_transaction.py
+++ app/transactions/logic2/dividend_transaction.py
@@ -4,21 +4,53 @@ from rates.models import CurrencyRate
 from files.models import ReportFile
 from utils.logic import get_previous_day_curreny_rate
 from utils.models import Broker
-from transactions.models import AssetTransaction
+from transactions.models import AssetTransaction, DividendTransaction
 from utils.choices import AssetType, TransactionSide, Currency
 from django.contrib.auth.models import User
 
-def save_ib_lynx_dividend_transaction(row: list[str], report_file_object: ReportFile):
-    asset_name_index = 5
-    asset_type_index = 3
-    price_index = 8
-    quantity_index = 7
-    value_index = 10
-    currency_index = 4
-    fee_index = 11
-    executed_at_index = 6
-    # NOTE finish it
+def _get_value_per_share(text: str) -> float | None:
+    # NOTE regex catching all the floating numbers from the string
+    match = re.search(re.compile(r'\b\d+(\.\d+)?\b'), text)
+    if match:
+        return float(match.group())
+    return None
 
+def save_ib_lynx_dividend_transaction(row: list[str], report_file_object: ReportFile):
+    asset_type_index = 0
+    asset_name_index = 4
+    value_index = 5
+    currency_index = 2
+    executed_at_index = 3
+
+    executed_at = datetime.strptime(row[executed_at_index], "%Y-%m-%d")
+    previous_day_currency_rate = CurrencyRate.objects.filter(date__lt=executed_at).order_by("-date").first()
+    
+    asset_type = (
+        row[asset_type_index]
+        .strip()
+    )
+    asset_name = row[asset_name_index].split("(")[0].strip()
+    currency = row[currency_index]
+    value_per_share = _get_value_per_share(row[asset_name_index])
+    value = round(float(row[value_index]), 2)
+    value_pln = (
+        round(value * getattr(previous_day_currency_rate, currency.lower()), 2) if currency.lower() != "pln" else value
+    )
+
+    DividendTransaction.objects.get_or_create(
+        asset_name=asset_name,
+        value_per_share=value_per_share,
+        value=value,
+        value_pln=value_pln,
+        executed_at=executed_at,
+        defaults={
+            "report_file": report_file_object,
+            "previous_day_currency_rate": previous_day_currency_rate,
+            "asset_type": asset_type,
+            "currency": currency,
+            "raw_data": str(row)
+        }
+    )
     # executed_at = datetime.strptime(row[executed_at_index], "%Y-%m-%d, %H:%M:%S") + timedelta(hours=6)
     # previous_day_currency_rate = get_previous_day_curreny_rate(executed_at)
 
