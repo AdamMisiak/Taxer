5418a2b8fb0661512a2f55727137fb94a145f15e
Adam Misiak <adammisiak3@gmail.com>
2024-04-21 11:27:04 +0200

    Clean up save_data_ib_lynx_report_file

--- app/files/logic/ib_lynx_report_file.py
+++ app/files/logic/ib_lynx_report_file.py
@@ -1,7 +1,5 @@
-from utils.choices import AssetType, TransactionSide, Currency
-from django.conf import settings
+from utils.choices import AssetType
 from files.models import ReportFile
-from utils.models import Broker
 import csv
 
 def _clean_up_row_ib_lynx_report_file(row: list[str]):
@@ -19,29 +17,24 @@ def _clean_up_row_ib_lynx_report_file(row: list[str]):
 
 def save_data_ib_lynx_report_file(file, report_file_object: ReportFile):
     from transactions.logic2 import save_ib_lynx_asset_transaction, save_ib_lynx_dividend_transaction, save_ib_lynx_withholding_tax_transaction, save_ib_lynx_option_transaction, save_ib_lynx_interest_rate_transaction_object
-    # from transactions.logic import save_withholding_tax_transaction_object_ib_broker, save_trade_transaction_object, save_dividend_transaction_object, save_interest_rates_transaction_object
 
     csvreader = csv.reader(file)
 
     for row in csvreader:
         row_type = row[0]
-
         # # NOTE  add script to make transactions chronigically here
-
-        # # NOTE oddzielnie zysk z opcji i akcji i oddzielnie dywidendy
+        
         # INTEREST RATES
         if row_type == "Interest" and row[1] == "Data" and "Investment Loan Interest for" in row[4]:
             save_ib_lynx_interest_rate_transaction_object(row, report_file_object)
 
         # ASSETS
         if row_type == "Trades" and row[1] == "Data" and row[3] in [AssetType.STOCKS.value]:
-            # print(row)
             _clean_up_row_ib_lynx_report_file(row)
             save_ib_lynx_asset_transaction(row, report_file_object)
 
         # OPTIONS
         if row_type == "Trades" and row[1] == "Data" and row[3] == "Equity and Index Options":
-            # print(row)
             _clean_up_row_ib_lynx_report_file(row)
             save_ib_lynx_option_transaction(row, report_file_object)
 
@@ -49,9 +42,6 @@ def save_data_ib_lynx_report_file(file, report_file_object: ReportFile):
         if row_type == AssetType.DIVIDENDS.value and row[1] == "Data" and not row[2].startswith("Total"):
             _clean_up_row_ib_lynx_report_file(row)
             save_ib_lynx_dividend_transaction(row, report_file_object)
-            # _validate_row_ib_broker_file(row)
-            # save_dividend_transaction_object(row)
-            # save_dividend_object(row)
 
         # WITHHOLDING TAXES
         elif row_type == AssetType.WITHHOLDING_TAX.value and row[1] == "Data" and not row[2].startswith("Total"):
