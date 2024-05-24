44850c347ccfdf8d16823dc3f3a51a57f38fe366
Adam Misiak <adammisiak3@gmail.com>
2024-05-24 19:09:36 +0200

    Add function calculate_tax_first_of_many_transactions_with_quantity

--- app/tax_calculations/logic/tax_calculations.py
+++ app/tax_calculations/logic/tax_calculations.py
@@ -58,10 +58,27 @@ def calculate_tax_multiple_transactions_different_quantity(
         quantity=quantity,
     )
 
-# TODO refactor this names
-def _get_partial_quantity_for_transaction(closing_transaction_quantity, summary_opening_transaction_quantity):
-    quantity_used_in_current_transaction = closing_transaction_quantity - summary_opening_transaction_quantity
-    return quantity_used_in_current_transaction
+def calculate_tax_first_of_many_transactions_with_quantity(
+    opening_transaction: BaseTransaction, closing_transaction: BaseTransaction, quantity: int = None
+):
+    ratio = quantity / opening_transaction.quantity
+    # tax_year = closing_transaction.executed_at.year or opening_transaction.executed_at.year
+
+    revenue = 0
+    cost = round(opening_transaction.full_value_pln * ratio, 2)
+    profit_or_loss = round(revenue - cost, 2)
+    tax_to_pay_from_transaction = round(profit_or_loss * settings.TAX_RATE, 2)
+
+    AssetTaxCalculation.objects.get_or_create(
+        # tax_summary=TaxSummary.objects.get(year=tax_year),
+        opening_transaction=opening_transaction,
+        closing_transaction=closing_transaction,
+        revenue=revenue,
+        cost=cost,
+        profit_or_loss=profit_or_loss,
+        tax=tax_to_pay_from_transaction,
+        quantity=quantity,
+    )
 
 def calculate_tax_multiple_transactions(matching_opening_transactions: QuerySet[BaseTransaction], closing_transaction: BaseTransaction):
     summary_opening_transactions_quantity = 0
@@ -78,6 +95,7 @@ def calculate_tax_multiple_transactions(matching_opening_transactions: QuerySet[
                 opening_transaction=opening_transaction, closing_transaction=closing_transaction
             )
             break
+
         elif opening_transaction.quantity <= closing_transaction.quantity and not opening_transaction.as_opening_calculation.all():
             if summary_opening_transactions_quantity + opening_transaction.quantity < closing_transaction.quantity:
                 summary_opening_transactions_quantity += opening_transaction.quantity
@@ -105,14 +123,14 @@ def calculate_tax_multiple_transactions(matching_opening_transactions: QuerySet[
             summary_opening_transactions_quantity += remaining_quantity
             print(f"ℹ️  Used (partial) first? transaction with smaller quantity: {opening_transaction}")
             print(f"ℹ️  Summary opening transactions quantity: {summary_opening_transactions_quantity}/{closing_transaction.quantity}")
-            # NOTE it  is not calcualting correclty -> revenue should be 0
-            # create another function calculate_tax_multiple_transactions_different_quantity but for FIRST trans and change current one to LAST trans?
-            calculate_tax_multiple_transactions_different_quantity(opening_transaction=opening_transaction, closing_transaction=closing_transaction, quantity=remaining_quantity)
+            calculate_tax_first_of_many_transactions_with_quantity(opening_transaction=opening_transaction, closing_transaction=closing_transaction, quantity=remaining_quantity)
             
 
-            # NOTE check if all transacations are covered
+            # NOTE check if all transacations are covered + proper checking for 2020-2023 transactions
         
-        # NOTE maybe check if there are as_opening_calculation and no quantity -> message that this transaction is already clacualted/addressed
+        elif opening_transaction.as_opening_calculation.all() and not opening_transaction.as_opening_calculation.last().quantity:
+            print(f"ℹ️  Transaction: {opening_transaction} was already used in the calculations!")
+
         else:
             # NOTE temp
             print(f"❌ Transaction: {opening_transaction} has not been handled!")
